2ba52197a29d2fdae6163daab172fe2db3136e4e
박현우 <ghwhsbsb123@gmail.com>
2023-12-30 16:26:56 +0900

    Add GameResultScreen.

--- lib/screens/game_result_screen.dart
+++ lib/screens/game_result_screen.dart
@@ -1,10 +1,107 @@
 import 'package:flutter/material.dart';
+import 'package:madcamp_week1/models/game_result.dart';
 
 class GameResultScreen extends StatelessWidget {
-  const GameResultScreen({super.key});
+  final List<GameResult> results;
+
+  const GameResultScreen({required this.results, super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    final corrects = results.where((e) => e.grade).length;
+
+    return Padding(
+      padding: const EdgeInsets.all(12),
+      child: Column(
+        children: [
+          const Text(
+            'Statistics',
+            style: TextStyle(
+              fontSize: 24,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.symmetric(vertical: 32),
+            child: Stack(
+              alignment: Alignment.center,
+              children: [
+                SizedBox(
+                  width: 128,
+                  height: 128,
+                  child: TweenAnimationBuilder<double>(
+                    curve: Curves.easeInOut,
+                    duration: const Duration(seconds: 1),
+                    tween: Tween(begin: 0, end: corrects / results.length),
+                    builder: (context, value, _) {
+                      return CircularProgressIndicator(
+                        backgroundColor: Colors.black26,
+                        strokeWidth: 8,
+                        value: value,
+                      );
+                    },
+                  ),
+                ),
+                Text(
+                  '$corrects / ${results.length}',
+                  style: const TextStyle(
+                    fontSize: 20,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+              ],
+            ),
+          ),
+          SingleChildScrollView(
+            scrollDirection: Axis.horizontal,
+            child: DataTable(
+              columnSpacing: 16,
+              headingTextStyle: const TextStyle(fontSize: 12),
+              dataTextStyle: const TextStyle(fontSize: 12),
+              columns: const [
+                DataColumn(label: Text('#')),
+                DataColumn(label: Text('Question')),
+                DataColumn(label: Text('Grade')),
+                DataColumn(label: Text('Your Answer')),
+                DataColumn(label: Text('Correct Answer')),
+              ],
+              rows: results.indexed
+                  .map<DataRow>((e) => _buildDataRow(e.$1, e.$2))
+                  .toList(),
+            ),
+          ),
+          const SizedBox(height: 8),
+          ElevatedButton.icon(
+            onPressed: () => Navigator.pop(context),
+            icon: const Icon(Icons.exit_to_app),
+            label: const Text('Exit'),
+          ),
+        ],
+      ),
+    );
+  }
+
+  DataRow _buildDataRow(int index, GameResult result) {
+    final style = TextStyle(color: result.grade ? Colors.blue : Colors.red);
+
+    return DataRow(
+      cells: [
+        DataCell(
+          Center(child: Text((index + 1).toString(), style: style)),
+        ),
+        DataCell(
+          Center(child: Text(result.question, style: style)),
+        ),
+        DataCell(
+          Center(child: Text(result.grade ? 'O' : 'X', style: style)),
+        ),
+        DataCell(
+          Center(child: Text(result.yourAnswer.toString(), style: style)),
+        ),
+        DataCell(
+          Center(child: Text(result.correctAnswer.toString(), style: style)),
+        ),
+      ],
+    );
   }
 }
