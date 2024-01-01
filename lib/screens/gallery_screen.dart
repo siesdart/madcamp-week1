9215134190d603b86c385e342711e0c8e9dcac95
sunnygeem <marguerite27@hanyang.ac.kr>
2024-01-01 13:17:16 +0900

    1/1 commit_2

--- lib/screens/gallery_screen.dart
+++ lib/screens/gallery_screen.dart
@@ -13,47 +13,166 @@ class _GalleryScreenState extends State<GalleryScreen> {
 
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.all(8),
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.stretch,
-        children: [
-          const Text(
-            'Images',
-            style: TextStyle(
-              fontSize: 20,
-              fontWeight: FontWeight.bold,
+
+    return Scaffold(
+      body: Stack(
+        children: <Widget>[
+          Positioned(
+            // background blue container
+            top: 0,
+            right: 0,
+            left: 0,
+            bottom: MediaQuery.of(context).size.height * 0.60,
+            // background blue container
+            child: Container(
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.only(
+                  bottomRight: Radius.circular(120.0),
+                ),
+                color: Colors.blueAccent,
+              ),
             ),
           ),
-          const SizedBox(height: 8),
-          Expanded(
-            child: GridView.count(
-              crossAxisCount: 3,
-              mainAxisSpacing: 4,
-              crossAxisSpacing: 4,
-              children: List.generate(20, _buildImageButton),
+          Positioned(
+            // main column1: every image
+            top: 15.0,
+            left: 20.0,
+            right: 20.0,
+            // main column1: every image
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: <Widget>[
+                const Text(
+                  'Images',
+                  style: TextStyle(
+                    fontSize: 26,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.white,
+                  ),
+                ),
+                Container(
+                  margin: EdgeInsets.only(top: 10.0),
+                  padding: EdgeInsets.all(20.0),
+                  height: 300.0,
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(38.0),
+                    color: Colors.white,
+                    //border: Border.all(color: Colors.blueGrey, width: 1),
+                    boxShadow: [
+                      BoxShadow(
+                        color: Colors.blueGrey.withOpacity(0.7),
+                        blurRadius: 5.0,
+                        spreadRadius: 0.0,
+                        offset: const Offset(0, 3),
+                      )
+                    ]
+                  ),
+                  child: (SingleChildScrollView(
+                    child: Column(
+                      children: [
+                        GridView.count(
+                          shrinkWrap: true,
+                          physics: ScrollPhysics(),
+                          scrollDirection: Axis.vertical,
+                          crossAxisCount: 3,
+                          mainAxisSpacing: 4,
+                          crossAxisSpacing: 4,
+                          children: List.generate(20, _buildImageButton),
+                        ),
+                      ],
+                    ),
+                  )
+                  )
+                ),
+              ],
             ),
           ),
-          const Divider(height: 32),
-          const Text(
-            'Liked Images',
-            style: TextStyle(
-              fontSize: 20,
-              fontWeight: FontWeight.bold,
+          Positioned(
+            // 2nd background container
+            top: 450,
+            right: 0,
+            left: 0,
+            bottom: 0,
+            // background blue container
+            child: Container(
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.only(
+                  topLeft: Radius.circular(120.0),
+                ),
+                color: Colors.blueAccent,
+              ),
             ),
           ),
-          const SizedBox(height: 8),
-          Expanded(
-            child: GridView.count(
-              crossAxisCount: 3,
-              mainAxisSpacing: 4,
-              crossAxisSpacing: 4,
-              children: _likedImages.map(_buildImageButton).toList(),
+          Positioned(
+            // main column2: liked image
+            top: 390,
+            left: 20,
+            right: 20,
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.end,
+              children: <Widget>[
+                const Text(
+                  'Liked Images',
+                  style: TextStyle(
+                    fontSize: 26,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.blueAccent,
+                  ),
+                ),
+                Container(
+                    margin: EdgeInsets.only(top: 10.0),
+                    padding: EdgeInsets.all(20.0),
+                    height: 250.0,
+                    decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(38.0),
+                        color: Colors.white,
+                        //border: Border.all(color: Colors.blueGrey, width: 1),
+                        boxShadow: [
+                          BoxShadow(
+                            color: Colors.blueGrey.withOpacity(0.7),
+                            blurRadius: 5.0,
+                            spreadRadius: 0.0,
+                            offset: const Offset(0, 3),
+                          )
+                        ]
+                    ),
+                    child: (SingleChildScrollView(
+                      child: Column(
+                        children: [
+                          GridView.count(
+                            physics: ScrollPhysics(),
+                            shrinkWrap: true,
+                            crossAxisCount: 3,
+                            mainAxisSpacing: 4,
+                            crossAxisSpacing: 4,
+                            children: _likedImages.map(_buildImageButton).toList(),
+                          ),
+                        ],
+                      ),
+                    )
+                    )
+                ),
+              ],
             ),
           ),
         ],
       ),
     );
+    // return Padding(
+    //   padding: const EdgeInsets.all(8),
+    //   child: Column(
+    //       const SizedBox(height: 8),
+    //       Expanded(
+    //         child: GridView.count(
+    //           crossAxisCount: 3,
+    //           mainAxisSpacing: 4,
+    //           crossAxisSpacing: 4,
+    //           children: _likedImages.map(_buildImageButton).toList(),
+    //         ),
+    //       ),
+    //     ],
+    //   ),
+    // );
   }
 
   Widget _buildImageButton(int index) {
