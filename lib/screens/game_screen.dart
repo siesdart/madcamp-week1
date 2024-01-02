b616af77024eb378ed5efa501a1e44238b134ca6
sunnygeem <marguerite27@hanyang.ac.kr>
2024-01-02 12:10:16 +0900

    1/2 commit_!

--- lib/screens/game_screen.dart
+++ lib/screens/game_screen.dart
@@ -69,140 +69,215 @@ class _GameScreenState extends State<GameScreen> {
     };
 
     return Scaffold(
-      body:
-        SingleChildScrollView(
-          child: Column(
-            children: [
-              SizedBox(
-                height: 10,
-              ),
-              Container(
-                decoration: BoxDecoration(
-                  image: DecorationImage(
-                    alignment: Alignment.center,
-                    fit: BoxFit.fitHeight,
-                    image: AssetImage('images/game_background1.jpg'),
+      body: Stack(
+          children: [
+            Positioned(
+              top: 0,
+              left: 0,
+              right: 0,
+              height: 192,
+              child: Container(
+                decoration: const BoxDecoration(
+                  borderRadius: BorderRadius.only(
+                    bottomRight: Radius.circular(120),
                   ),
+                  color: Colors.blueAccent,
                 ),
+              ),
+            ),
+            Positioned(
+              child: SingleChildScrollView(
                 child: Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
                   children: [
                     SizedBox(
-                      height: 30,
+                      height: 10,
                     ),
-                    const Text(
-                      'Calculating Game',
-                      style: TextStyle(
-                        fontWeight: FontWeight.bold,
-                        fontSize: 28,
-                        color: Colors.blueAccent,
-                      ),
-                    ),
-                    Text(
-                      'Current level: $_level',
-                      style: const TextStyle(
-                        fontSize: 16,
-                        color: Colors.black54,
-                      ),
-                    ),
-                    const SizedBox(height: 32),
-                    Row(
+                    Column(
                       mainAxisAlignment: MainAxisAlignment.center,
-                      crossAxisAlignment: CrossAxisAlignment.end,
                       children: [
-                        Text(
-                          '$operand1 $operator $operand2 = ',
-                          style: const TextStyle(fontSize: 20),
-                        ),
                         SizedBox(
-                          width: 180,
-                          child: TextField(
-                            style: const TextStyle(
-                              fontSize: 18,
-                              color: Colors.black,
+                          height: 30,
+                        ),
+                        const Text(
+                          'Calculating Game',
+                          style: TextStyle(
+                            fontWeight: FontWeight.bold,
+                            fontSize: 32,
+                            color: Colors.white,
+                          ),
+                        ),
+                        const SizedBox(height: 20),
+                        Container(
+                          width: 400,
+                          height: 40,
+                          color: Colors.transparent,
+                          alignment: Alignment.center,
+                          child: Scaffold(
+                            backgroundColor: Colors.transparent,
+                            body: Stack(
+                              children: [
+                                Positioned(
+                                  left: 30,
+                                  right: 30,
+                                  child: Container(
+                                    padding: EdgeInsets.only(
+                                      left: 20,
+                                      right: 20,
+                                    ),
+                                    width: 300,
+                                    height: 30,
+                                    decoration: const BoxDecoration(
+                                      borderRadius: BorderRadius.all(Radius.circular(30)),
+                                      color: Colors.white38,
+                                    ),
+                                    child: Row(
+                                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                                      children: [
+                                        Text(
+                                            'level1',
+                                          style: TextStyle(
+                                            //fontWeight: FontWeight.bold,
+                                          ),
+                                        ),
+                                        Text(
+                                          'level2',
+                                          style: TextStyle(
+                                          //fontWeight: FontWeight.bold,
+                                        ),),
+                                        Text(
+                                            'level3',
+                                          style: TextStyle(
+                                            //fontWeight: FontWeight.bold,
+                                          ),
+                                        ),
+                                      ],
+                                    ),
+                                  ),
+                                ),
+                                AnimatedPositioned(
+                                  top: 0,
+                                  left: (_level == 1) ? 30 : (_level == 2) ? 150 : (_level == 3) ? 263 : 0,
+                                  duration: const Duration(seconds: 2),
+                                  curve: Curves.ease,
+                                  child: Container(
+                                      width: 100,
+                                      height: 30,
+                                      decoration: const BoxDecoration(
+                                        borderRadius: BorderRadius.all(Radius.circular(30)),
+                                        color: Colors.black12,
+                                      ),
+                                  ),
+                                ),
+                              ],
                             ),
-                            decoration: const InputDecoration(
-                              isDense: true,
-                              hintText: 'Enter your answer',
+                          ),
+                        ),
+                        // Text(
+                        //   'Current level: $_level',
+                        //   style: const TextStyle(
+                        //     fontSize: 16,
+                        //     color: Colors.black54,
+                        //   ),
+                        // ),
+                        const SizedBox(height: 60),
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          crossAxisAlignment: CrossAxisAlignment.end,
+                          children: [
+                            Text(
+                              '$operand1 $operator $operand2 = ',
+                              style: const TextStyle(fontSize: 20),
                             ),
-                            textAlign: TextAlign.center,
-                            onSubmitted: (str) {
-                              final userAns = int.tryParse(str);
-                              if (userAns == null) return;
+                            SizedBox(
+                              width: 180,
+                              child: TextField(
+                                style: const TextStyle(
+                                  fontSize: 18,
+                                  color: Colors.black,
+                                ),
+                                decoration: const InputDecoration(
+                                  isDense: true,
+                                  hintText: 'Enter your answer',
+                                ),
+                                textAlign: TextAlign.center,
+                                onSubmitted: (str) {
+                                  final userAns = int.tryParse(str);
+                                  if (userAns == null) return;
 
-                              setState(() {
-                                checkAnswer(answer, str);
-                                addToGameResult(
-                                  operand1,
-                                  operand2,
-                                  operator,
-                                  userAns,
-                                  answer,
-                                );
-                              });
-                            },
+                                  setState(() {
+                                    checkAnswer(answer, str);
+                                    addToGameResult(
+                                      operand1,
+                                      operand2,
+                                      operator,
+                                      userAns,
+                                      answer,
+                                    );
+                                  });
+                                },
+                              ),
+                            ),
+                          ],
+                        ),
+                        const SizedBox(height: 20),
+                        ElevatedButton.icon(
+                          onPressed: _gameResults.isEmpty
+                              ? null
+                              : () => setState(() => _isExited = true),
+                          icon: const Icon(Icons.exit_to_app),
+                          label: const Text('Exit'),
+                        ),
+                        const SizedBox(height: 80),
+                        Container(
+                          decoration: BoxDecoration(
+                            color: Colors.white70,
+                          ),
+                          padding: const EdgeInsets.all(12),
+                          child: const Text.rich(
+                            TextSpan(
+                              style: TextStyle(
+                                fontSize: 13,
+                                color: Colors.black54,
+                              ),
+                              children: [
+                                TextSpan(
+                                  text: 'How To Play\n',
+                                  style: TextStyle(fontWeight: FontWeight.bold),
+                                ),
+                                TextSpan(
+                                  text: '1. Enter the answer to the given question.\n',
+                                ),
+                                TextSpan(
+                                  text:
+                                  '2.\n    2-1. If your answer is CORRECT, you\'ll see "Success!"\n',
+                                ),
+                                TextSpan(
+                                  text:
+                                  '    2-2. If your answer is INCORRECT, you\'ll see "Fail.."\n',
+                                ),
+                                TextSpan(
+                                  text:
+                                  '3. If you get 3 questions correct in current level, you can move on to the next level.\n',
+                                ),
+                                TextSpan(
+                                  text:
+                                  '    3-1. If you get even ONE QUESTION WRONG, you will be returned to the previous level.\n      If the current level is 1, you start from the beginning.\n',
+                                ),
+                                TextSpan(
+                                  text: '    3-2. There are levels up to 3.',
+                                ),
+                              ],
+                            ),
                           ),
                         ),
                       ],
                     ),
-                    const SizedBox(height: 20),
-                    ElevatedButton.icon(
-                      onPressed: _gameResults.isEmpty
-                          ? null
-                          : () => setState(() => _isExited = true),
-                      icon: const Icon(Icons.exit_to_app),
-                      label: const Text('Exit'),
-                    ),
-                    const SizedBox(height: 200),
-                    Container(
-                      decoration: BoxDecoration(
-                        color: Colors.white70,
-                      ),
-                      padding: const EdgeInsets.all(12),
-                      child: const Text.rich(
-                        TextSpan(
-                          style: TextStyle(
-                            fontSize: 13,
-                            color: Colors.black54,
-                          ),
-                          children: [
-                            TextSpan(
-                              text: 'How To Play\n',
-                              style: TextStyle(fontWeight: FontWeight.bold),
-                            ),
-                            TextSpan(
-                              text: '1. Enter the answer to the given question.\n',
-                            ),
-                            TextSpan(
-                              text:
-                              '2.\n    2-1. If your answer is CORRECT, you\'ll see "Success!"\n',
-                            ),
-                            TextSpan(
-                              text:
-                              '    2-2. If your answer is INCORRECT, you\'ll see "Fail.."\n',
-                            ),
-                            TextSpan(
-                              text:
-                              '3. If you get 3 questions correct in current level, you can move on to the next level.\n',
-                            ),
-                            TextSpan(
-                              text:
-                              '    3-1. If you get even ONE QUESTION WRONG, you will be returned to the previous level.\n      If the current level is 1, you start from the beginning.\n',
-                            ),
-                            TextSpan(
-                              text: '    3-2. There are levels up to 3.',
-                            ),
-                          ],
-                        ),
-                      ),
-                    ),
                   ],
                 ),
               ),
-            ],
-          ),
+            ),
+          ],
         ),
-
     );
   }
 
